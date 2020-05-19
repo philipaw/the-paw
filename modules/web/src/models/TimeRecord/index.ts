cc02b806ed2511c32ad90dff5fc82bf0a7c63df7
philipnb <philip@getnabis.com>
2020-05-19 13:08:42 -0700

    added frontend graphql docs for timeRecord

---
+++ modules/web/src/models/TimeRecord/index.ts
@@ -0,0 +1,3 @@
+import { loader } from 'graphql.macro'
+export const allTimeRecordsQueryDoc = loader('./AllTimeRecords.graphql')
+export const createTimeRecordMutationDoc = loader('./AllTimeRecords.graphql')
